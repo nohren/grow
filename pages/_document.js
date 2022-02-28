8fb4f981e824b2f5a4ab67e94c30f05412d44701
nohren <orendnelson@gmail.com>
2022-02-28 01:06:00 -0500

    added a bigger title, added precursors to mobile compati bility

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,14 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
             rel="stylesheet"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=0.67, user-scalable=0"
           />
         </Head>
         <body>
